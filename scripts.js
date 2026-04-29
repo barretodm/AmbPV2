0fcd11498defdb75942b397ea81ff4259c2b0169
barretodm <danielbarretu@gmail.com>
2026-04-29 18:13:22 -0300

    adicionado scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Soma de números e impressão
+function somarNumeros(a, b) {
+  const resultado = a + b;
+  console.log(`${a} + ${b} = ${resultado}`);
+  return resultado;
+}
+
+// Exemplo de uso
+somarNumeros(5, 3);
+somarNumeros(10, 20);
+somarNumeros(100, 50);
